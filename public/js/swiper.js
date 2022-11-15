9d40042a5a9105ea44b8202d6cadd0e997320bb5
lehinfo <lehinfo.felix@gmail.com>
2022-11-15 14:38:11 -0300

    refatoracao 0.5

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -3,9 +3,9 @@ var swiper = new Swiper(".mySwiper", {
         loop: true,
         slidesPerView: 3,
         spaceBetween: 30,
-  slidesPerGroup: 1,
+        slidesPerGroup: 1,
         autoplay: {
-          delay: 1000,
+          delay: 2300,
         },
         loopFillGroupWithBlank: true,
         pagination: {
